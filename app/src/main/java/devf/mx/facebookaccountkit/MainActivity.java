c4b26625254c92fef33993b8f74e3c1ead36205c
KenMarquez <darius_dam@hotmail.com>
2016-11-28 00:27:48 -0600

    Verificamos que el token de la sesión activa es valido

--- app/src/main/java/devf/mx/facebookaccountkit/MainActivity.java
+++ app/src/main/java/devf/mx/facebookaccountkit/MainActivity.java
@@ -1,12 +1,20 @@
 package devf.mx.facebookaccountkit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.facebook.accountkit.AccessToken;
 import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int APP_REQUEST_CODE = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +23,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
